4b984d57e63719956e9577a37e2ce9e68e4f85f2
Michele Lana <36988839+skippydream@users.noreply.github.com>
2026-02-23 14:55:28 +0100

    yo

--- python/gis_website/settings.py
+++ python/gis_website/settings.py
@@ -1,24 +1,30 @@
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = 'my-secret-key'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+# Queste variabili vengono lette dalle environment variables di Koyeb
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-change-this')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
 LOGIN_URL = '/login'
-if os.environ["PRODUCTION"] == '1':
+
+# Impostazioni di sicurezza per la produzione
+if os.environ.get("PRODUCTION") == '1':
     DEBUG = False
-    ALLOWED_HOSTS = ['gis.example.org']
-    SECRET_KEY = os.environ["SECRET_KEY"]
     SECURE_HSTS_SECONDS = 3600
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
-
+    # Necessario per far capire a Django che l'SSL è gestito dal proxy (Koyeb)
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
@@ -62,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gis_website.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -72,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -92,23 +96,22 @@ AUTH_PASSWORD_VALIDATORS = [
     }
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.parent / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.parent / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
